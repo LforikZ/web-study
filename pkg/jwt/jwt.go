609f8afe0554b6907d533c6d078f4120b5c61b77
lizi <1143997956@qq.com>
2023-01-17 13:25:33 +0800

    2023/1/16

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -23,34 +23,61 @@ type MyClaims struct {
 }
 
 // GenToken 生成JWT
-func GenToken(username string, userID int64) (string, error) {
+func GenToken(username string, userID int64) (aToken, rToken string, err error) {
 	// 创建一个我们自己的声明
 	c := MyClaims{
-		userID,
-		username, // 自定义字段
+		userID, // 自定义字段
+		username,
 		jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(TokenExpireDuration).Unix(), // 过期时间
+			ExpiresAt: time.Now().Add(TokenExpireDuration).Unix(), // 过期时间  2小时
 			Issuer:    "web-study",                                // 签发人
 		},
 	}
-	// 使用指定的签名方法创建签名对象
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, c)
-	// 使用指定的secret签名并获得完整的编码后的字符串token  加盐
-	return token.SignedString(MySecret)
+	// 使用指定的签名方法创建签名对象 atoken
+	aToken, err = jwt.NewWithClaims(jwt.SigningMethodHS256, c).SignedString(MySecret)
+
+	// refresh token 不需要任何自定义数据
+	rToken, err = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(time.Second * 30).Unix(), //过期时间 30天
+		Issuer:    "web-study",                             //签发人
+	}).SignedString(MySecret)
+
+	return
 }
 
 // ParseToken 解析JWT
-func ParseToken(tokenString string) (*MyClaims, error) {
+func ParseToken(tokenString string) (claims *MyClaims, err error) {
 	// 解析token
-	var mc = new(MyClaims)
-	token, err := jwt.ParseWithClaims(tokenString, &MyClaims{}, func(token *jwt.Token) (i interface{}, err error) {
-		return MySecret, nil
-	})
+	var token *jwt.Token
+	claims = new(MyClaims)
+	token, err = jwt.ParseWithClaims(tokenString, claims, Keyfunc)
 	if err != nil {
 		return nil, err
 	}
-	if token.Valid { //校验token
-		return mc, nil
+	if !token.Valid { //校验token
+		err = errors.New("invalid token")
 	}
-	return nil, errors.New("invalid token")
+	return
+}
+
+// TODO: 这个功能没有实现，学习完之后需要自己完成
+func RefreshToken(aToken, rToken string) (newAToken, newRToken string, err error) {
+	// refresh token 无效直接返回
+	if _, err = jwt.Parse(rToken, Keyfunc); err != nil {
+		return
+	}
+
+	//从旧的access token 中解析出claims数据
+	var claims MyClaims
+	_, err = jwt.ParseWithClaims(aToken, &claims, Keyfunc)
+	v, _ := err.(*jwt.ValidationError)
+
+	//当access token是过期错误 并且 refresh token没有过期时 就创建一个新的access token
+	if v.Errors == jwt.ValidationErrorExpired {
+		return GenToken(claims.Username, claims.UserId)
+	}
+	return
+}
+func Keyfunc(token *jwt.Token) (interface{}, error) {
+	return "", errors.New("123")
 }
